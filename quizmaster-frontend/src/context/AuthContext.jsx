392921eb116bbddc25fb3ee3ff98f7142ad1c2ad
Rauan Zholaman <rauankairav@gmail.com>
2025-11-08 22:55:41 +0800

    Add a state variable and logiv for storing first name of the user

--- quizmaster-frontend/src/context/AuthContext.jsx
+++ quizmaster-frontend/src/context/AuthContext.jsx
@@ -10,23 +10,39 @@ export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     if (!auth) {
       setLoading(false);
       return;
     }
+
+
     const unsub = onAuthStateChanged(auth, async (u) => {
       setUser(u || null);
+
+
       try {
         if (u) {
           const snap = await getDoc(doc(db, "users", u.uid));
+          const data = snap.exists() ? snap.data() : null;
+
           setRole(snap.exists() ? snap.data().role : null);
+
+          setProfile({
+            firstName: data.firstName || "",
+            lastName: data.lastName || "",
+            ...data
+          })
         } else {
+          // Profile and Role resets on logout
           setRole(null);
+          setProfile(null);
         }
       } catch {
         setRole(null);
+        setProfile(null);
       } finally {
         setLoading(false);
       }
@@ -35,7 +51,7 @@ export default function AuthProvider({ children }) {
   }, []);
 
   return (
-    <AuthCtx.Provider value={{ user, role, loading }}>
+    <AuthCtx.Provider value={{ user, role, loading, profile }}>
       {!loading && children}
     </AuthCtx.Provider>
   );
